ee976f4a2b7857ef95c0c65abc37eacc26873713
Tim <tim.fulton@state.mn.us>
2024-12-10 15:27:17 -0600

    add shinylive

---
+++ steps.R
@@ -0,0 +1,22 @@
+# Steps to deploy Shiny App on GitHub Pages
+
+# 00. GitHub repo has already been created and cloned.
+
+# 01. Create an app: Copy "Hello World" from `{shiny}`
+
+system.file("examples", "01_hello", package="shiny") |>
+  fs::dir_copy("app", overwrite = TRUE)
+
+# 02. Export to Shinylive app
+
+# Load the shinylive package
+library(shinylive)
+
+# Export the app
+shinylive::export("app", "docs")  # This can take a few minutes
+
+# 03. Check the app is working 
+
+httpuv::runStaticServer("docs")
+ 
+# Adding comment
